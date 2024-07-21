681b4928e6886cf9a7eda3519fa85c4254b48761
Mohammed Alazami <moh-azamy@hotmail.com>
2024-07-21 09:52:32 +0300

    connect to mongoDB Atlas

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect('mongodb://localhost:27017/userDB');
+mongoose.connect(process.env.CONNECT_TO_MONGOATLAS);
 
 const userSchema = new mongoose.Schema({
     email: String,
